f2c2ad0ae8aa709e21296c167cb9598a2b6b78d4
tonnyogeto <tonnymaishaogeto@gmail.com>
2024-07-31 12:25:58 +0300

    refactoring endpoints

--- src/main/java/com/tony/QuizApp/controller/QuestionController.java
+++ src/main/java/com/tony/QuizApp/controller/QuestionController.java
@@ -10,27 +10,27 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("questions")
+@RequestMapping("api/questions")
 public class QuestionController {
     @Autowired
     QuestionService questionService;
 
-    @GetMapping("allQuestions")
+    @GetMapping
     public ResponseEntity<List<Question>> getAllQuestions() {
         return questionService.getAllQuestions();
     }
 
-    @GetMapping("category/{category}")
+    @GetMapping("/{category}")
     public ResponseEntity<List<Question>> getQuestionByCategory(@PathVariable("category") String category){
         return questionService.getQuestionByCategory(category);
     }
 
-    @PostMapping("add")
+    @PostMapping
     public ResponseEntity<String> addQuestion(@RequestBody Question question){
         return questionService.addQuestion(question);
     }
 
-    @PutMapping("{questionId}")
+    @PutMapping("/{questionId}")
     public String updateQuestion(
             @PathVariable("questionId") Integer questionId,
             @RequestBody Question question
@@ -38,7 +38,7 @@ public class QuestionController {
         return questionService.updateQuestion(question, questionId);
     }
 
-    @DeleteMapping("{questionId}")
+    @DeleteMapping("/{questionId}")
     public String deleteProduct(
             @PathVariable("questionId") Integer questionId
     ){
--- src/main/java/com/tony/QuizApp/controller/QuizController.java
+++ src/main/java/com/tony/QuizApp/controller/QuizController.java
@@ -10,17 +10,17 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("quiz")
+@RequestMapping("api/quiz")
 public class QuizController{
 
     @Autowired
     QuizService quizService;
 
-    @PostMapping("create")
+    @PostMapping("/create")
     public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title){
         return quizService.createQuiz(category, numQ, title);
     }
-    @GetMapping("get/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<List<QuestionDTO>> getQuizQuestions(@PathVariable Integer id){
         return quizService.getQuizQuestions(id);
     }
